f90adebdfa2faac513d376d7a04345a14b2f304d
Pete Corey <petecorey@gmail.com>
2022-10-23 21:46:42 -0600

    Debug

--- lib/cloud_watch.ex
+++ lib/cloud_watch.ex
@@ -31,7 +31,7 @@ defmodule CloudWatch do
         %{level: min_level, metadata_filter: metadata_filter} = state
       ) do
     if Logger.compare_levels(level, min_level) != :lt and metadata_matches?(md, metadata_filter) do
-      state
+      {:ok, state}
       # |> add_message(level, msg, ts, md)
       # |> flush()
     else
