a0871e7daf7dfcf0910a6662ed787e798384f6bb
Pete Corey <petecorey@gmail.com>
2022-10-24 14:58:01 -0600

    Debug

--- lib/cloud_watch.ex
+++ lib/cloud_watch.ex
@@ -30,8 +30,6 @@ defmodule CloudWatch do
         {level, _gl, {Logger, msg, ts, md}},
         %{level: min_level, metadata_filter: metadata_filter} = state
       ) do
-    IO.puts("handle_event")
-
     if Logger.compare_levels(level, min_level) != :lt and metadata_matches?(md, metadata_filter) do
       {:ok, state}
       # |> add_message(level, msg, ts, md)
@@ -42,6 +40,7 @@ defmodule CloudWatch do
   end
 
   def handle_event(:flush, state) do
+    IO.puts("handle_event :flush")
     {:ok, purge_buffer(state)}
   end
 
@@ -51,6 +50,7 @@ defmodule CloudWatch do
   end
 
   def handle_info(:flush, state) do
+    IO.puts("handle_info :flush")
     {:ok, flushed_state} = flush(state, force: true)
     Process.send_after(self(), :flush, state.max_timeout)
     {:ok, flushed_state}
